e08bd4827eec43b48693bc98ee0c280eaacd426b
Nassehk <nassehk@gmail.com>
2020-07-27 20:46:16 -0400

    Bug fixes, refer to change log

---
+++ V2.3/Results_xlsx_save_v4.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 30 09:07:26 2015
+
+Cross platform (Windows and Linux compatible)
+@author: nasseh
+"""
+from collections import OrderedDict
+global sr, reg_order, interval
+#this sript saves the results of calculation in the xlsx format
+def saver(file_list,time_cond_master, temp_cond_master, dil_cond_master,
+result_master_time, result_master_temp, result_master_dil, result_master_fraction, result_master_ID,result_master_FD_fraction, result_master_C_in_FD_fraction, result_master_C_in_FD_WP, result_master_cem_fraction,result_master_Vol_f_transformed, result_master_FD_vol,result_master_cem_vol,result_master_aus_vol, run_parameters):
+    global sr, reg_order, interval
+    
+    from openpyxl import Workbook
+    import os
+    import time;
+    import sys
+#    import pickle
+    
+    current_dir=os.getcwd()
+#    file_list=pickle.load(open(current_dir+'/working_directory/file_list_conditioned.pkl','rb'))
+    A=os.listdir(current_dir)
+    if 'Results' not in A:
+        if sys.platform[:3]=='win':
+            os.makedirs(current_dir+'\\Results')
+        else:
+            os.makedirs(current_dir+'/Results')
+    
+    wb = Workbook()
+    wb.remove_sheet(wb.get_sheet_by_name("Sheet"))
+    ws = wb.create_sheet(title="Run parameters")
+
+    i=0
+    keys=["script_name","sr","reg_order","interval","optimized","L0_Correction",    
+    "end_fit_WF","overal_fit_WF","err_end_slope_WF","err_maximum_transformation_WF",
+    'Bs_model', "Ms_model","a0_gama","a0_alpha","CTE_alpha_a","CTE_alpha_b","CTE_alpha_c", "c_wf_for_cte","c_wf_for_a0","use avr CTE product for initial quess of CTE_alpha"]   
+    for key in keys:
+        i=i+1
+        ws.cell(row=i, column=1, value=key)
+        ws.cell(row=i, column=2, value=run_parameters[key])  
+        
+    Columns=OrderedDict([("Time_conditioned(s)",time_cond_master),
+              ("Temp_conditioned(C)", temp_cond_master),
+              ("dil_conditioned(m)", dil_cond_master),
+              ("time_result(s)", result_master_time),
+              ("Temp_result(C)", result_master_temp),
+              ("dil_result(m)", result_master_dil),
+              ("Mole fraction transformed", result_master_fraction),
+              ("Volume fraction transformed", result_master_Vol_f_transformed),
+              ("phase ID", result_master_ID),
+              ("Mole fraction FD formed", result_master_FD_fraction),
+              ("Volume fraction FD formed",result_master_FD_vol),
+              ("C in FD(mole fraction)", result_master_C_in_FD_fraction),
+              ("C in FD(W%)", result_master_C_in_FD_WP),
+              ("Fe in cementite(mole fraction)", result_master_cem_fraction)])
+              
+              
+    #, "Temp_conditioned(C)","dil_conditioned(m)", "time_result(s)", "Temp_result(C)","dil_result(m)","fraction transformed","phase ID", "fraction FD formed",}
+    for i in range(len(file_list)):
+        ws = wb.create_sheet(title=file_list[i][0])
+        
+        for j in range(len(Columns.keys())):
+            #print len(Collumns)
+            ws.cell(row=1,column=j+1,value=list(Columns.keys())[j])    
+            for row in range(2,len(Columns[list(Columns.keys())[j]][i])+2):
+                ws.cell(row=row, column=j+1, value=Columns[list(Columns.keys())[j]][i][row-2])
+        
+    wintime=time.strftime("%Y.%m.%d - %H.%M")
+
+    Result_file_name=wintime+".xlsx"
+    if sys.platform[:3]=='win':
+        wb.save(current_dir+'\\Results\\'+Result_file_name)
+    else:
+        wb.save(current_dir+'/Results/'+Result_file_name)
+
+    #print "***888****8888****"
+    #print (current_dir+'/Results/'+Result_file_name)
+    file_location=str('./Results/'+Result_file_name)
+    return file_location
+    #call([ 'open file_location'])
+#    call(['open', file_location])
+#    os.sys('open 12.xlsx')
---
+++ V2.3/data_set_conditioner_V4.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 23 03:55:32 2015
+
+
+@author: nasseh
+"""
+
+# This program is called data_set_conditioner. It gets rid noise and excessive data point.
+
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.polynomial import polynomial as P
+import pickle
+import os
+
+#Load setup file which contains information that master_fitter and fitter needs to work.
+#each line in the set file is like this:
+#filename,Aus_min_row,Aus_max_row,fer_min_row,fer_max_row,L0,cementite conditions,
+#values must be separated by comma "," and all lines must end with a comma.
+make_raw_data_plots = False
+file_list=[]
+d=open('master_setup.txt','r')
+for line in d:
+    temp=line.split(',')
+    temp[1:6]=map(float,temp[1:6]) # get rid of end character and turn numbers into float
+    if temp[0][0]=='#' or temp[0][0]=='%': #this if statement ignors the lines in master_setup file that begin with '#' or '%'.
+        pass
+    else:
+        file_list.append(temp)
+
+current_dir=os.getcwd()
+A=os.listdir(current_dir)
+if 'working_directory' not in A:        
+    os.makedirs(current_dir+'/working_directory')
+
+output = open(current_dir+'/working_directory/file_list_conditioned.pkl', 'wb')    
+pickle.dump(file_list, output)      
+output.close()  
+
+time_master=[]
+temp_master=[]
+dil_master=[]
+k=0
+
+for var in file_list:
+#    if sys.platform[0:5]=='linux':
+#        try:
+#            Data=open('/media/nasseh/FE0E012C0E00E00F/Science_Backup/Results/Python_Dil_analysis/Fitting_real_function/'+var[0],'rb')
+#        except:
+#            Data=open('/home/nasseh/Desktop/Python_Dil_analysis/Fitting_real_function/'+var[0],'rb')
+#    else:
+#        Data =  open('G:\\Science_Backup\\Results\\Python_Dil_analysis\\Fitting_real_function\\'+var[0], 'rb')
+    
+    Data=open(var[0],'r')
+    D= csv.reader(Data)
+    t = []
+    temp = []
+    dil = []
+    for row in D:
+        t.append(row[0])
+        temp.append(row[1])
+        dil.append(row[2])
+    del t[0],temp [0] ,dil [0]
+    Data.close()
+
+    t = np.array(t, dtype = 'float')
+    temp = np.array(temp, dtype = 'float')
+    dil =np.array(dil, dtype = 'float')*1E-6
+    time_master.append(t)
+    temp_master.append(temp)
+    dil_master.append(dil)
+    del t, temp, dil, row
+
+reg_order=2 #The order of polynomial used in noise filtering. Higher sr better have higher reg-order
+def regresser(bottom,center,top):
+    time_reg=time_fit[center]
+    coef1= P.polyfit(time_fit[bottom:top],temp_fit[bottom:top],reg_order)
+    temp_reg=P.polyval(time_fit[center],coef1)
+    coef1= P.polyfit(time_fit[bottom:top],dil_fit[bottom:top],reg_order)
+    dil_reg=P.polyval(time_fit[center],coef1)
+    regressed_point=[time_reg,temp_reg,dil_reg]
+    return regressed_point
+
+#def range_finder(T,dT)
+
+start=0
+time_cond=[] #conditioned time data
+temp_cond=[]
+dil_cond=[]
+
+time_cond_master=[] #master file that has all of the conditioned time data
+temp_cond_master=[]
+dil_cond_master=[]
+
+regressed_point_list=[]
+
+for i in range(len(time_master)):
+    print (i)
+    time_cond.append(time_master[i][0])
+    temp_cond.append(temp_master[i][0])
+    dil_cond.append(dil_master[i][0])
+    time_fit=time_master[i]
+    dil_fit=dil_master[i]
+    temp_fit=temp_master[i]
+    points=[]
+    for j in range(1,len(time_master[i])):
+        try:        
+            reged_point=regresser(j-20,j,j+20)
+        except:
+#            print"out"
+            reged_point=[time_fit[j],temp_fit[j],dil_fit[j]]
+        if abs(time_fit[j]-time_cond[-1])>=10:
+            points.append(j)
+            time_cond.append(time_fit[j])          
+        elif abs(reged_point[1]-temp_cond[-1])>=3:
+            points.append(j)
+            temp_cond.append(temp_fit[j])
+    regressed_point_list.append(points)   
+time_cond=[]
+temp_cond=[]
+dil_cond=[]         
+for i in range(len(time_master)):
+    time_cond=[]
+    temp_cond=[]
+    dil_cond=[]    
+    time_cond.append(time_master[i][0])
+    temp_cond.append(temp_master[i][0])
+    dil_cond.append(dil_master[i][0])
+    time_fit=time_master[i]
+    dil_fit=dil_master[i]
+    temp_fit=temp_master[i]
+    points=regressed_point_list[i]
+    for j in range(1,len(points)-1):
+        regressed=regresser(points[j-1],points[j],points[j+1])
+        time_cond.append(regressed[0])
+        temp_cond.append(regressed[1])
+        dil_cond.append(regressed[2])
+    time_cond_master.append(time_cond) 
+    temp_cond_master.append(temp_cond)
+    dil_cond_master.append(dil_cond)
+
+
+output = open(current_dir+'/working_directory/time_conditioned.pkl', 'wb')    
+pickle.dump(time_cond_master, output)      
+output.close()        
+
+output = open(current_dir+'/working_directory/temp_conditioned.pkl', 'wb')    
+pickle.dump(temp_cond_master, output)      
+output.close()
+
+output = open(current_dir+'/working_directory/dil_conditioned.pkl', 'wb')    
+pickle.dump(dil_cond_master, output)      
+output.close()  
+    
+if make_raw_data_plots:    
+    plt.figure(figsize=(8,6))
+    for i in range(len(file_list)):
+        plt.plot(time_master[i],1e6*dil_master[i], label="Raw "+file_list[i][0])
+        
+        
+    for i in range(len(file_list)):
+        plt.plot(time_cond_master[i],1e6*np.array(dil_cond_master[i]),".", label="Conditioned "+ file_list[i][0])
+    plt.legend()
+    plt.title("Original data vs conditioned")
+    plt.xlabel("Time $(s)$")
+    plt.ylabel("Dilation $(\mu m)$")
+    
+    plt.figure(figsize=(8,6))
+    
+    plt.title("Original data vs conditioned")
+    plt.xlabel("Temperature $(\degree C)$")
+    plt.ylabel("Dilation $(\mu m)$")
+    
+    for i in range(len(file_list)):
+        plt.plot(temp_master[i],1e6*dil_master[i], label="Raw "+file_list[i][0])
+        
+    for i in range(len(file_list)):
+        plt.plot(temp_cond_master[i],1e6*np.array(dil_cond_master[i]),".", label="Conditioned "+file_list[i][0])
+    plt.legend()
+    
+    plt.figure(figsize=(8,6))
+    plt.title("Original data vs conditioned")
+    plt.xlabel("Time $(s)$")
+    plt.ylabel("Temperature $(\degree C)$")
+    
+    for i in range(len(file_list)):
+        plt.plot(time_master[i],temp_master[i],label=file_list[i][0])
+        
+    for i in range(len(file_list)):
+        plt.plot(time_cond_master[i],temp_cond_master[i],"o", label=file_list[i][0])
+    plt.legend()
+    #plt.show()
+
